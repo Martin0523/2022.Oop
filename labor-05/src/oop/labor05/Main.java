c8e200ee9c70257e0785dee1e52c6d8d2dd12787
Martin0523 <m.martin_2010@yahoo.com>
2022-03-16 14:11:39 +0200

    Lab5 Course, Student, Training

--- labor-05/src/oop/labor05/Main.java
+++ labor-05/src/oop/labor05/Main.java
@@ -1,7 +1,65 @@
 package oop.labor05;
 
+import oop.labor05.models.Course;
+import oop.labor05.models.Student;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        ArrayList<Course> courses = readCourses("courses.csv");
+        for (Course course:courses){
+            System.out.println(course);
+        }
+
+        ArrayList<Student> students = readStudents("students.csv");
+        for (Student student:students){
+            System.out.println(student);
+        }
+    }
+
+    private static ArrayList<Course> readCourses(String filename){
+        ArrayList<Course> courses = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(filename))){
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.isEmpty()){
+                    continue;
+                }
+                String[] items = line.split(",");
+                String courseName = items[0].trim();
+                String description = items[1].trim();
+                int numHours = Integer.parseInt(items[2].trim());
+                courses.add(new Course(courseName, description, numHours));
+            }
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return courses;
+    }
+
+    private static ArrayList<Student> readStudents(String filename){
+        ArrayList<Student> students = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(filename))){
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.isEmpty()){
+                    continue;
+                }
+                String[] items = line.split(",");
+                String ID = items[0].trim();
+                String firstName = items[1].trim();
+                String lastName = items[2].trim();
+                students.add(new Student(ID, firstName, lastName));
+            }
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return students;
     }
 }
---
+++ labor-05/src/oop/labor05/models/Training.java
@@ -0,0 +1,81 @@
+package oop.labor05.models;
+
+import oop.labor02.date.MyDate;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+public class Training {
+    private final Course course;
+    private final MyDate startDate;
+    private final MyDate endDate;
+    private final double pricePerStudent;
+    private ArrayList<Student> enrolledStudents;
+
+    public Training(Course course, MyDate startDate, MyDate endDate, double pricePerStudent) {
+        this.course = course;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.pricePerStudent = pricePerStudent;
+    }
+
+    public boolean enroll(Student student){
+        for (Student s:enrolledStudents){
+            if (s.getID().equals(student.getID())){
+                return false;
+            }
+        }
+        enrolledStudents.add(student);
+        return true;
+    }
+
+    public Student findStudentById(String ID){
+        for (Student s:enrolledStudents){
+            if (s.getID().equals(ID)){
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public int numEnrolled(){
+        return enrolledStudents.size();
+    }
+
+    public void printToFile(){
+        String dateStart = startDate.getYear() + "." + startDate.getMonth() + "." + startDate.getDay();
+        String dateEnd = endDate.getYear() + "." + endDate.getMonth() + "." + endDate.getDay();
+        String fileName = String.format("%s_%s_%s", course.getName(), dateStart, dateEnd);
+        try(PrintStream output = new PrintStream(){
+
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Training{" +
+                "\n\tcourse=" + course +
+                "\n\tstartDate=" + startDate +
+                "\n\tendDate=" + endDate +
+                "\n\t\tenrolledStudents=" + enrolledStudents +
+                '}';
+    }
+
+    public void unEnroll(String ID){
+        for (int i = 0; i < enrolledStudents.size(); i++) {
+            if (enrolledStudents.get(i).getID().equals(ID)){
+                enrolledStudents.remove(i);
+                return;
+            }
+        }
+    }
+}
