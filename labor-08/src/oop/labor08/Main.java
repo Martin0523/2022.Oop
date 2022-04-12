939a9f0d8531870347701077e0ed605d014e254b
Martin0523 <m.martin_2010@yahoo.com>
2022-04-12 16:35:18 +0300

    Lab 8 Bank and Stack main

--- labor-08/src/oop/labor08/Main.java
+++ labor-08/src/oop/labor08/Main.java
@@ -4,31 +4,56 @@ import oop.labor08.lab8_1.*;
 import oop.labor08.lab8_2.StackAggregation;
 import oop.labor08.lab8_2.StackInheritance;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-/*
+
         Bank bank1 = new Bank("OTP");
         Customer customer1 = new Customer("Customer", "One");
         Customer customer2 = new Customer("Customer", "Two");
+        bank1.addCustomer(customer1);
+        bank1.addCustomer(customer2);
+
         customer1.addAccount(new SavingsAccount(1.2));
         customer1.addAccount(new CheckingAccount(30));
         customer2.addAccount(new SavingsAccount(1.4));
         customer2.addAccount(new CheckingAccount(60));
 
-//        for (int cid: bank1.get)
 
-        //hianyzik a customerbol a getAccountNumbers()
-
-//        customer1.getAccountNumbers();
-
-//        customer1.getAccount("OTP0000001").deposit(100);
-//        customer1.getAccount("OTP0000002").deposit(32);
-//        customer2.getAccount("OTP0000003").deposit(150);
-//        customer2.getAccount("OTP0000004").deposit(56);
+        for (int i = 0; i < bank1.numCostumers(); i++){
+            ArrayList<String> accId = bank1.getCustomer(i).getAccountNumbers();
+            for (String id:accId){
+                bank1.getCustomer(i).getAccount(id).deposit(100);
+            }
+        }
 
         System.out.println(customer1);
         System.out.println(customer2);
-        */
+
+        for (int i = 0; i < bank1.numCostumers(); i++){
+            ArrayList<String> accId = bank1.getCustomer(i).getAccountNumbers();
+            for (String id:accId){
+                if (bank1.getCustomer(i).getAccount(id) instanceof SavingsAccount){
+                    ((SavingsAccount) bank1.getCustomer(i).getAccount(id)).addInterest();
+                }
+            }
+        }
+
+        System.out.println(customer1);
+        System.out.println(customer2);
+
+        for (int i = 0; i < bank1.numCostumers(); i++){
+            ArrayList<String> accId = bank1.getCustomer(i).getAccountNumbers();
+            for (String id:accId){
+                bank1.getCustomer(i).getAccount(id).withdraw(145);
+            }
+        }
+
+        System.out.println(customer1);
+        System.out.println(customer2);
+
+        System.out.println('\n');
 
         StackAggregation stack1 = new StackAggregation( 5 );
         for( int i=0; i<10; ++i ){
