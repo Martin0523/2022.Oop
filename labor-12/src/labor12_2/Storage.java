af940b0b1ce4786f2156a5ce01ef0940372d1467
Martin0523 <m.martin_2010@yahoo.com>
2022-05-15 19:09:26 +0300

    Lab12_3 Storage hashmap

---
+++ labor-12/src/labor12_2/Storage.java
@@ -0,0 +1,88 @@
+package labor12_2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Storage {
+//    private ArrayList<Product> products = new ArrayList<>();
+    private HashMap<Integer, Product> products = new HashMap<>();
+
+    public Storage(String fileName) {
+        try (Scanner scanner = new Scanner((new File("Data/"+fileName)))){
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.isEmpty()) continue;
+                String[] items = line.split(" ");
+                int ID = Integer.parseInt(items[0].trim());
+                String name = items[1].trim();
+                int amount = Integer.parseInt(items[2].trim());
+                int price = Integer.parseInt(items[3].trim());
+                products.put(ID, new Product(ID, name, amount, price));
+//                products.add(new Product(ID, name, amount, price));
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+//        products.sort(Product::compareTo);
+
+    }
+
+    public void update(String fileName){
+        int updated = 0;
+        try (Scanner scanner = new Scanner(new File("Data/"+fileName))) {
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if (line.isEmpty()) continue;
+                String[] items = line.split(" ");
+                int ID = Integer.parseInt(items[0].trim());
+                //van beépített binary search
+//                int index = binarySearch(products, ID);
+//                if (index != -1){
+//                    products.get(index).increaseAmount(Integer.parseInt(items[1].trim()));
+//                    updated++;
+//                }
+                try{
+                    products.get(ID).increaseAmount(Integer.parseInt(items[1].trim()));
+                    updated++;
+                }
+                catch (Exception e){
+
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Number of products that were successfully updated: " + updated);
+    }
+
+    public int binarySearch(ArrayList<Product> products, int ID){
+        int left = 0;
+        int right = products.size()-1;
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            if (products.get(mid).getID() == ID){
+                return mid;
+            }
+            if (products.get(mid).getID() < ID){
+                left = mid +1;
+            }
+            else {
+                right = mid -1;
+            }
+        }
+        return -1;
+    }
+
+    public void print(){
+//        for (Product p:products){
+//            System.out.println(p);
+//        }
+        for (Map.Entry<Integer, Product> entry : products.entrySet()){
+            System.out.println(entry.getValue());
+        }
+    }
+}
